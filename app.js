502e7b30bfc0d45db7060cf5c45a25809ff0160d
Ismail Arilik <ismail.arilik@piworks.net>
2020-08-28 12:09:41 +0300

    Fix linter errors

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
-var app = express();
+var app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-module.exports = app;
+module.exports = app
