e803a43d726d2ff479306634c4517e71ef7e3b66
Ismail Arilik <ismail.arilik@piworks.net>
2020-08-29 21:16:07 +0300

    Write comments

--- routes/images.js
+++ routes/images.js
@@ -5,23 +5,28 @@ const path = require('path')
 const fs = require('fs')
 
 const upload = multer({
+  // The directory for uploaded images
   dest: './public/images'
 })
 
 const directoryPath = path.join(__dirname, '..', 'public', 'images')
 
+// POST to /images to upload images
 router.post('/', upload.single('file'), (req, res) => {
   res.json({ files: req.files })
 });
 
+// GET to /images to get an array of images
 router.get('/', (req, res) => {
   fs.readdir(directoryPath, function (err, files) {
-    //handling error
+    // Handle errors
     if (err) {
+      console.log(`Unable to scan directory: ${err}`)
       res.status(500).send(`Unable to scan directory: ${err}`)
     } else {
-    const filesSent = files.map(file => `images/${file}`)
-    res.json(filesSent)
+      // Sent image relative paths
+      const filesSent = files.map(file => `images/${file}`)
+      res.json(filesSent)
     }
   });
 });
