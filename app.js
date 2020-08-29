eb06fae47df554485d62fe3038c6d7d2716ea603
Ismail Arilik <ismail.arilik@piworks.net>
2020-08-29 19:12:35 +0300

    Write upload controller

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 var express = require('express')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+const multer = require('multer')
+const cors = require('cors');
 
 var app = express()
 
+const upload = multer({
+  dest: './public/images'
+})
+app.use(cors());
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.json({ files: req.files })
+});
 module.exports = app
