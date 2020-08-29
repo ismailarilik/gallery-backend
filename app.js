de8a5def5bd4eacf117cfc691b5e51a542269851
Ismail Arilik <ismail.arilik@piworks.net>
2020-08-29 19:34:40 +0300

    Modularize and make restful routes

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 var express = require('express')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
-const multer = require('multer')
-const cors = require('cors');
 const path = require('path')
-const fs = require('fs')
+const cors = require('cors');
+
+var imagesRouter = require('./routes/images');
 
 var app = express()
-
-const upload = multer({
-  dest: './public/images'
-})
-
-const directoryPath = path.join(__dirname, 'public', 'images')
 
 app.use(cors());
 app.use(logger('dev'))
@@ -21,19 +15,6 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/upload', upload.single('file'), (req, res) => {
-  res.json({ files: req.files })
-});
-
-app.get('/images', (req, res) => {
-  fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }
-    const filesSent = files.map(file => `images/${file}`)
-    res.json(filesSent)
-  });
-});
+app.use('/images', imagesRouter);
 
 module.exports = app
---
+++ routes/images.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
+
+const upload = multer({
+  dest: './public/images'
+})
+
+const directoryPath = path.join(__dirname, '..', 'public', 'images')
+
+router.post('/', upload.single('file'), (req, res) => {
+  res.json({ files: req.files })
+});
+
+router.get('/', (req, res) => {
+  fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    const filesSent = files.map(file => `images/${file}`)
+    res.json(filesSent)
+  });
+});
+
+module.exports = router;
