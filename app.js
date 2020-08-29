6cc76911c68749b6bb65f20b5d133529daf2ac46
Ismail Arilik <ismail.arilik@piworks.net>
2020-08-29 19:13:18 +0300

    Write images controller which is responsible for serving images

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 const multer = require('multer')
 const cors = require('cors');
+const path = require('path')
+const fs = require('fs')
 
 var app = express()
 
 const upload = multer({
   dest: './public/images'
 })
+
+const directoryPath = path.join(__dirname, 'public', 'images')
+
 app.use(cors());
 app.use(logger('dev'))
 app.use(express.json())
@@ -19,4 +24,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.post('/upload', upload.single('file'), (req, res) => {
   res.json({ files: req.files })
 });
+
+app.get('/images', (req, res) => {
+  fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    const filesSent = files.map(file => `images/${file}`)
+    res.json(filesSent)
+  });
+});
+
 module.exports = app
